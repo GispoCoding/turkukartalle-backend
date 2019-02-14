368da619321c222b9c7a3a3fc5eeabf6bfb94b8b
Erno Mäkinen <ernoma@gmail.com>
2019-02-14 10:43:54 +0200

    Enable CORS

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const client = new postgres.Client({ connectionString });
 
 client.connect();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', (req, res) => res.send('Turku kartalle backend is alive!'))
 
 app.get('/features', (req, res) => {
